a36fa0d9a9e686340dc2d818f1e838fb5630de56
yulias <yulia@Yulias-MacBook-Air.local>
2017-07-07 23:09:12 +0300

    refactoring

--- server.py
+++ server.py
@@ -5,22 +5,28 @@ my_flask_app = Flask(__name__)
 
 @my_flask_app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title="Карточки для изучения иностранного языка")
 
 
 @my_flask_app.route('/new/')
 def all_news():
-    return render_template('new_card.html')
+    return render_template('new_card.html', title="Создание новой карточки", 
+        nav_link_1="/cards/", nav_link_2="/training/", 
+        nav_link_1_name="Все карточки", nav_link_2_name="Тренировка")
 
 
 @my_flask_app.route('/cards/')
 def all_cards():
-    return render_template('all_cards.html')
+    return render_template('all_cards.html', title="Все карточки", 
+        nav_link_1="/new/", nav_link_2="/training/", 
+        nav_link_1_name="Создание новой карточки", nav_link_2_name="Тренировка")
 
 
 @my_flask_app.route('/training/')
 def training():
-    return render_template('training.html')       
+    return render_template('training.html', title="Тренировка", 
+        nav_link_1="/new/", nav_link_2="/cards/", 
+        nav_link_1_name="Создание новой карточки", nav_link_2_name="Все карточки")       
 
 
 if __name__ == "__main__":
