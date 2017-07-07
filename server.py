789bdaa967633ed6c535958de3f6e68ce3845326
yulias <yulia@Yulias-MacBook-Air.local>
2017-07-07 14:02:55 +0300

    new links and pages

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 my_flask_app = Flask(__name__)
 
@@ -8,10 +8,20 @@ def index():
     return render_template('index.html')
 
 
+@my_flask_app.route('/new/')
+def all_news():
+    return render_template('new_card.html')
+
+
 @my_flask_app.route('/cards/')
 def all_cards():
-    return render_template('all_cards.html')    
+    return render_template('all_cards.html')
+
+
+@my_flask_app.route('/training/')
+def training():
+    return render_template('training.html')       
 
 
 if __name__ == "__main__":
-    my_flask_app.run(debug=True)
+    my_flask_app.run(debug=True)   
