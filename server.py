b7b8f205dcf1b158f495576bde0837d073f1e692
yulias <yulia@Yulias-MacBook-Air.local>
2017-07-08 13:09:49 +0300

    pep8 and other small fixes

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 my_flask_app = Flask(__name__)
 
@@ -10,24 +10,30 @@ def index():
 
 @my_flask_app.route('/new/')
 def all_news():
-    return render_template('new_card.html', title="Создание новой карточки", 
+    return render_template(
+        'new_card.html', title="Создание новой карточки",
         nav_link_1="/cards/", nav_link_2="/training/", 
-        nav_link_1_name="Все карточки", nav_link_2_name="Тренировка")
+        nav_link_1_name="Все карточки", nav_link_2_name="Тренировка"
+    )
 
 
 @my_flask_app.route('/cards/')
 def all_cards():
-    return render_template('all_cards.html', title="Все карточки", 
+    return render_template(
+        'all_cards.html', title="Все карточки",
         nav_link_1="/new/", nav_link_2="/training/", 
-        nav_link_1_name="Создание новой карточки", nav_link_2_name="Тренировка")
+        nav_link_1_name="Создание новой карточки", nav_link_2_name="Тренировка"
+    )
 
 
 @my_flask_app.route('/training/')
 def training():
-    return render_template('training.html', title="Тренировка", 
-        nav_link_1="/new/", nav_link_2="/cards/", 
-        nav_link_1_name="Создание новой карточки", nav_link_2_name="Все карточки")       
+    return render_template(
+        'training.html', title="Тренировка",
+        nav_link_1="/new/", nav_link_2="/cards/",
+        nav_link_1_name="Создание новой карточки", nav_link_2_name="Все карточки"
+    )
 
 
 if __name__ == "__main__":
-    my_flask_app.run(debug=True)   
+    my_flask_app.run(debug=True)
